cc5b587bc3dfa1d2fd1da44c72883af53246a23c
Khoa Hoang <tuankhoa.0013@gmail.com>
2019-06-30 11:33:34 +0700

    make it better

--- public/js/history.js
+++ public/js/history.js
@@ -2,11 +2,17 @@ $(function () {
     $('#search').click(function () {
         $('#tableHistory').html(`<table id="dataHistory" class="table table-striped text-center"><thead class="thead-dark"><tr><th scope="col">Ngày</th><th scope="col">Tháng</th><th scope="col">Năm</th><th scope="col">Số giờ làm</th></tr></thead><tbody></tbody></table>`)
         $.get(`/history?qDate=${$('#fDate').val()}&qMonth=${$('#fMonth').val()}&qYear=${$('#fYear').val()}`, function (res) {
-            res.forEach(element => {
+            if (res.length == 0) {
                 $('#dataHistory').append(function () {
-                    return `<tr><td>${element.date}</td><td>${element.month}</td><td>${element.year}</td><td>${element.noHours}</td></tr>`
+                    return `<tr><td>Hông có</td><td>Hông có</td><td>Hông có</td><td>Hông có</td></tr>`
                 })
-            })
+            } else {
+                res.forEach(element => {
+                    $('#dataHistory').append(function () {
+                        return `<tr><td>${element.date}</td><td>${element.month}</td><td>${element.year}</td><td>${element.noHours}</td></tr>`
+                    })
+                })
+            }
         })
     })
     $('#searchAll').click(function () {
--- routes/routes.js
+++ routes/routes.js
@@ -112,17 +112,69 @@ module.exports = function (app, passport) {
         }
         // thang nam
         if (req.query.qDate == '' & req.query.qMonth != '' & req.query.qYear != '') {
-            await Trucs.find({ month: req.query.qMonth, year: req.query.qYear }, function (err, results) {
-                if (err) {
-                    throw err
+            var dataMonth = req.query.qMonth
+            var dataLastMonth = req.query.qMonth - 1
+            var dataYear = req.query.qYear
+            var dataLastYear = req.query.qYear - 1
+            if (dataMonth > 1) {
+                for (var i = 21; i < 32; i++) {
+                    await Trucs.find({ date: i, month: dataLastMonth, year: dataYear }, function (err, truc) {
+                        if (err) {
+                            throw err
+                        } else {
+                            if (truc.length > 0) {
+                                temp_history.push({ 'date': truc[0].date, 'month': truc[0].month, 'year': truc[0].year, 'noHours': truc[0].noHours })
+                            }
+                        }
+                    })
                 }
-                results.forEach(function (row) {
-                    temp_history.push({ 'date': row.date, 'month': row.month, 'year': row.year, 'noHours': row.noHours })
-                })
-                res.status(200).send(temp_history)
-            })
+                for (var i = 1; i < 21; i++) {
+                    await Trucs.find({ date: i, month: dataMonth, year: dataYear }, function (err, truc) {
+                        if (err) {
+                            throw err
+                        } else {
+                            if (truc.length > 0) {
+                                temp_history.push({ 'date': truc[0].date, 'month': truc[0].month, 'year': truc[0].year, 'noHours': truc[0].noHours })
+                            }
+                        }
+                    })
+                }
+            }
+            if (dataMonth == 1) {
+                for (var i = 21; i < 32; i++) {
+                    await Trucs.find({ date: i, month: 12, year: dataLastYear }, function (err, truc) {
+                        if (err) {
+                            throw err
+                        } else {
+                            if (truc.length > 0) {
+                                temp_history.push({ 'date': truc[0].date, 'month': truc[0].month, 'year': truc[0].year, 'noHours': truc[0].noHours })
+                            }
+                        }
+                    })
+                }
+                for (var i = 1; i < 21; i++) {
+                    await Trucs.find({ date: i, month: dataMonth, year: dataYear }, function (err, truc) {
+                        if (err) {
+                            throw err
+                        } else {
+                            if (truc.length > 0) {
+                                temp_history.push({ 'date': truc[0].date, 'month': truc[0].month, 'year': truc[0].year, 'noHours': truc[0].noHours })
+                            }
+                        }
+                    })
+                }
+            }
+            // await Trucs.find({ month: req.query.qMonth, year: req.query.qYear }, function (err, results) {
+            //     if (err) {
+            //         throw err
+            //     }
+            //     results.forEach(function (row) {
+            //         temp_history.push({ 'date': row.date, 'month': row.month, 'year': row.year, 'noHours': row.noHours })
+            //     })
+            //     res.status(200).send(temp_history)
+            // })
+            res.status(200).send(temp_history)
         }
-
     })
 }
 
