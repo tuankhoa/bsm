b118fa6093bf4d51bed4e5d198bc812094c42924
tuankhoa <tuankhoa.0013@gmail.com>
2020-02-26 04:38:00 +0700

    edit code download

--- api/communications/proccessGetData.js
+++ api/communications/proccessGetData.js
@@ -3,13 +3,12 @@ var request = require('request')
 
 module.exports = function (io) {
     io.on('connection', function (socket) {
-        socket.on('getData', async function (data) {
-            var lines = JSON.parse((fs.readFileSync(`./jsonFull/HCL/newLines.json`)).toString())
+        socket.on('getTask', async (data) => {
             var emptyArrTask = true
             var tasksLine = []
             var pageTask = 0
             while (emptyArrTask) {
-                var currentTasks = JSON.parse(await doRequest(tasksGetOption(1, pageTask)))
+                var currentTasks = JSON.parse(await doRequest(tasksGetOption(data, pageTask)))
                 if (currentTasks.length) {
                     currentTasks.forEach(task => {
                         tasksLine.push(task)
@@ -19,29 +18,48 @@ module.exports = function (io) {
                     emptyArrTask = false
                 }
             }
-            socket.emit('resData', JSON.stringify(tasksLine))
-            // lines.forEach(async line => {
-            //     var emptyArrTask = true
-            //     var tasksLine = []
-            //     var pageTask = 0
-            //     while (emptyArrTask) {
-            //         var currentTasks = JSON.parse(await doRequest(tasksGetOption(line.id, constant.token, pageTask)))
-            //         if (currentTasks.length) {
-            //             currentTasks.forEach(task => {
-            //                 tasksLine.push(task)
-            //             })
-            //             pageTask++
-            //         } else {
-            //             emptyArrTask = false
-            //         }
-            //     }
-            //     socket.emit('resData', JSON.stringify(tasksLine))
-            // })
+            socket.emit('resTask', tasksLine)
         })
+
+        socket.on('getCalendar', async (data) => {
+            var emptyArrCalendar = true
+            var components = []
+            var pageCalendar = 0
+            while (emptyArrCalendar) {
+                var currentComponents = JSON.parse(await doRequest(calendarGetOption(data.lineId, data.machineId, pageCalendar)))
+                if (currentComponents.length) {
+                    currentComponents.forEach(async component => {
+                        components.push(component)
+                    })
+                    pageCalendar++
+                } else {
+                    emptyArrCalendar = false
+                }
+            }
+            socket.emit('resCalendar', components)
+        })
+        // socket.on('getData', async function (data) {
+        //     var lines = JSON.parse((fs.readFileSync(`./jsonFull/HCL/newLines.json`)).toString())
+        //     var emptyArrTask = true
+        //     var tasksLine = []
+        //     var pageTask = 0
+        //     while (emptyArrTask) {
+        //         var currentTasks = JSON.parse(await doRequest(tasksGetOption(1, pageTask)))
+        //         if (currentTasks.length) {
+        //             currentTasks.forEach(task => {
+        //                 tasksLine.push(task)
+        //             })
+        //             pageTask++
+        //         } else {
+        //             emptyArrTask = false
+        //         }
+        //     }
+        //     socket.emit('resData', tasksLine)
+        // })
     })
 }
 
-function tasksGetOption (lineId, page) {
+function tasksGetOption(lineId, page) {
     return {
         'method': 'GET',
         'url': `https://pmweb.az.team/api/tasks?lineId.equals=${lineId}&done.equals=false&late.equals=false&weekFrom.greaterOrEqualThan=2018-12-31T00:00:00.000Z&weekTo.lessOrEqualThan=2020-02-23T23:59:59.999Z&size=2000&page=${page}&sort=time,asc&sort=id,asc`,
@@ -57,7 +75,23 @@ function tasksGetOption (lineId, page) {
     }
 }
 
-function doRequest (option) {
+function calendarGetOption(lineId, machineId, page) {
+    return {
+        'method': 'GET',
+        'url': `https://pmweb.az.team/api/calendar/components?groupDetails.equals=false&enableGroupDetails.equals=false&breakdowns.equals=false&time.greaterOrEqualThan=2018-12-31T00:00:00.000Z&time.lessOrEqualThan=2020-02-23T23:59:59.999Z&lineId.equals=${lineId}&machineId.equals=${machineId}&page=${page}&size=2000`,
+        'headers': {
+            'Authorization': `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0dWFua2hvYSIsImF1dGgiOiJST0xFX0FETUlOLFJPTEVfU1VQRVJVU0VSLFJPTEVfVVNFUiIsInBsYW50IjoyLCJleHAiOjE1ODUxNDE2NDJ9.jy9Iq23ejD_UAq1vWGiIQdG0AXYpBfbLPB8q2LKVS69CHBrhxjbRL_6QKSkr0wGpLlwYbnpcCcdhPwVOefJTxA`,
+            'User-Agent': 'Mozilla/5.0 (compatible; Rigor/1.0.0; http://rigor.com)',
+            'Accept': '*/*',
+            'Cache-Control': 'no-cache',
+            // 'Postman-Token': 'd2938c16-cd54-405f-aef8-65e7cac8b76a',
+            'Host': 'pmweb.az.team',
+            'Connection': 'keep-alive'
+        }
+    }
+}
+
+function doRequest(option) {
     return new Promise((resolve, reject) => {
         request(option, function (error, res, body) {
             if (!error && res.statusCode == 200) {
--- public/jsClient/getData.js
+++ public/jsClient/getData.js
@@ -1,11 +1,125 @@
 var socket = io()
 
-$('#getData').click(function () {
-    socket.emit('getData', 0)
+var tempText
+
+// 1111111111111111
+$('#getTask1').click(function () {
+    tempText = $('#line1')
+    var lineId = 0
+    linesFoods.forEach(l => {
+        if (l.count == $('#line1').val()) {
+            lineId = l.id
+        }
+    })
+    socket.emit('getTask', lineId)
+})
+$('#getCalendar1').click(function () {
+    tempText = $('#machine1')
+    var data = { lineId: 0, machineId: 0 }
+    machinesFoods.forEach(m => {
+        if (m.count == $('#machine1').val()) {
+            data.lineId = m.line.id
+            data.machineId = m.id
+        }
+    })
+    socket.emit('getCalendar', data)
+})
+// 22222222222222222
+$('#getTask2').click(function () {
+    tempText = $('#line2')
+    var lineId = 0
+    linesHcl.forEach(l => {
+        if (l.count == $('#line2').val()) {
+            lineId = l.id
+        }
+    })
+    socket.emit('getTask', lineId)
+})
+$('#getCalendar2').click(function () {
+    tempText = $('#machine2')
+    var data = { lineId: 0, machineId: 0 }
+    machinesHcl.forEach(m => {
+        if (m.count == $('#machine2').val()) {
+            data.lineId = m.line.id
+            data.machineId = m.id
+        }
+    })
+    socket.emit('getCalendar', data)
+})
+// 33333333333333333
+$('#getTask3').click(function () {
+    tempText = $('#line3')
+    var lineId = 0
+    linesOral.forEach(l => {
+        if (l.count == $('#line3').val()) {
+            lineId = l.id
+        }
+    })
+    socket.emit('getTask', lineId)
+})
+$('#getCalendar3').click(function () {
+    tempText = $('#machine3')
+    var data = { lineId: 0, machineId: 0 }
+    machinesOral.forEach(m => {
+        if (m.count == $('#machine3').val()) {
+            data.lineId = m.line.id
+            data.machineId = m.id
+        }
+    })
+    socket.emit('getCalendar', data)
+})
+// 4444444444444444444
+$('#getTask4').click(function () {
+    tempText = $('#line4')
+    var lineId = 0
+    linesPcl.forEach(l => {
+        if (l.count == $('#line4').val()) {
+            lineId = l.id
+        }
+    })
+    socket.emit('getTask', lineId)
+})
+$('#getCalendar4').click(function () {
+    tempText = $('#machine4')
+    var data = { lineId: 0, machineId: 0 }
+    machinesPcl.forEach(m => {
+        if (m.count == $('#machine4').val()) {
+            data.lineId = m.line.id
+            data.machineId = m.id
+        }
+    })
+    socket.emit('getCalendar', data)
+})
+// 55555555555555555555555
+$('#getTask5').click(function () {
+    tempText = $('#line5')
+    var lineId = 0
+    linesUtility.forEach(l => {
+        if (l.count == $('#line5').val()) {
+            lineId = l.id
+        }
+    })
+    socket.emit('getTask', lineId)
+})
+$('#getCalendar5').click(function () {
+    tempText = $('#machine5')
+    var data = { lineId: 0, machineId: 0 }
+    machinesUtility.forEach(m => {
+        if (m.count == $('#machine5').val()) {
+            data.lineId = m.line.id
+            data.machineId = m.id
+        }
+    })
+    socket.emit('getCalendar', data)
 })
 
-socket.on('resData', function (res) {
-    downloadJsonFile(res, 'data')
+socket.on('resTask', function (data) {
+    downloadJsonFile(data, `${tempText.val()}`)
+    tempText.val(`${parseInt(tempText.val()) + 1}`)
+})
+socket.on('resCalendar', function (data) {
+    downloadJsonFile(data, `${tempText.val()}`)
+    tempText.val(`${parseInt(tempText.val()) + 1}`)
 })
 
 function downloadJsonFile(obj, fileName) {
