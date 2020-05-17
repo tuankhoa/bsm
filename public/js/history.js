344794f3dc26b1a7ba0dbd347d1d9b82299e1f7b
tuankhoa <tuankhoa.0013@gmail.com>
2020-05-17 18:55:35 +0700

    add columns STT

--- public/js/history.js
+++ public/js/history.js
@@ -1,26 +1,26 @@
 $(function () {
     $('#search').click(function () {
-        $('#tableHistory').html(`<table id="dataHistory" class="table table-striped text-center"><thead class="thead-dark"><tr><th scope="col">Ngày</th><th scope="col">Tháng</th><th scope="col">Năm</th><th scope="col">Số giờ làm</th></tr></thead><tbody></tbody></table>`)
+        $('#tableHistory').html(`<table id="dataHistory" class="table table-striped text-center"><thead class="thead-dark"><tr><th scope="col">STT</th><th scope="col">Ngày</th><th scope="col">Tháng</th><th scope="col">Năm</th><th scope="col">Số giờ làm</th></tr></thead><tbody></tbody></table>`)
         $.get(`/history?qDate=${$('#fDate').val()}&qMonth=${$('#fMonth').val()}&qYear=${$('#fYear').val()}`, function (res) {
             if (res.length == 0) {
                 $('#dataHistory').append(function () {
-                    return `<tr><td>Hông có</td><td>Hông có</td><td>Hông có</td><td>Hông có</td></tr>`
+                    return `<tr><td>#</td><td>Hông có</td><td>Hông có</td><td>Hông có</td><td>Hông có</td></tr>`
                 })
             } else {
-                res.forEach(element => {
+                res.forEach((element, i) => {
                     $('#dataHistory').append(function () {
-                        return `<tr><td>${element.date}</td><td>${element.month}</td><td>${element.year}</td><td>${element.noHours}</td></tr>`
+                        return `<tr><td>${i + 1}</td><td>${element.date}</td><td>${element.month}</td><td>${element.year}</td><td>${element.noHours}</td></tr>`
                     })
                 })
             }
         })
     })
     $('#searchAll').click(function () {
-        $('#tableHistory').html(`<table id="dataHistory" class="table table-striped text-center"><thead class="thead-dark"><tr><th scope="col">Ngày</th><th scope="col">Tháng</th><th scope="col">Năm</th><th scope="col">Số giờ làm</th></tr></thead><tbody></tbody></table>`)
+        $('#tableHistory').html(`<table id="dataHistory" class="table table-striped text-center"><thead class="thead-dark"><tr><th scope="col">STT</th><th scope="col">Ngày</th><th scope="col">Tháng</th><th scope="col">Năm</th><th scope="col">Số giờ làm</th></tr></thead><tbody></tbody></table>`)
         $.get(`/historyall`, function (res) {
-            res.forEach(element => {
+            res.forEach((element, i) => {
                 $('#dataHistory').append(function () {
-                    return `<tr><td>${element.date}</td><td>${element.month}</td><td>${element.year}</td><td>${element.noHours}</td></tr>`
+                    return `<tr><td>${i + 1}</td><td>${element.date}</td><td>${element.month}</td><td>${element.year}</td><td>${element.noHours}</td></tr>`
                 })
             })
         })
