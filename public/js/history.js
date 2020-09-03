7175d997e1e42c5e3b7aa2267facf8d9a8f2ba29
tuankhoa <tuankhoa.0013@gmail.com>
2020-09-03 21:34:24 +0700

    add  table-bordered

--- public/js/history.js
+++ public/js/history.js
@@ -1,6 +1,6 @@
 $(function () {
     $('#search').click(function () {
-        $('#tableHistory').html(`<table id="dataHistory" class="table table-striped text-center"><thead class="thead-dark"><tr><th scope="col">STT</th><th scope="col">Ngày</th><th scope="col">Tháng</th><th scope="col">Năm</th><th scope="col">Số giờ làm</th><th scope="col">Ghi chú</th></tr></thead><tbody></tbody></table>`)
+        $('#tableHistory').html(`<table id="dataHistory" class="table table-striped table-bordered text-center"><thead class="thead-dark"><tr><th scope="col">STT</th><th scope="col">Ngày</th><th scope="col">Tháng</th><th scope="col">Năm</th><th scope="col">Số giờ làm</th><th scope="col">Ghi chú</th></tr></thead><tbody></tbody></table>`)
         $.get(`/history?qDate=${$('#fDate').val()}&qMonth=${$('#fMonth').val()}&qYear=${$('#fYear').val()}`, function (res) {
             if (res.length == 0) {
                 $('#dataHistory').append(function () {
@@ -16,7 +16,7 @@ $(function () {
         })
     })
     $('#searchAll').click(function () {
-        $('#tableHistory').html(`<table id="dataHistory" class="table table-striped text-center"><thead class="thead-dark"><tr><th scope="col">STT</th><th scope="col">Ngày</th><th scope="col">Tháng</th><th scope="col">Năm</th><th scope="col">Số giờ làm</th><th scope="col">Ghi chú</th></tr></thead><tbody></tbody></table>`)
+        $('#tableHistory').html(`<table id="dataHistory" class="table table-striped table-bordered text-center"><thead class="thead-dark"><tr><th scope="col">STT</th><th scope="col">Ngày</th><th scope="col">Tháng</th><th scope="col">Năm</th><th scope="col">Số giờ làm</th><th scope="col">Ghi chú</th></tr></thead><tbody></tbody></table>`)
         $.get(`/historyall`, function (res) {
             res.forEach((element, i) => {
                 $('#dataHistory').append(function () {
